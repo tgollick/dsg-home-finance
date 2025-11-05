8a54ce524f14eebd680dbea1bc1ed79ddf47d53d
Tom Gollick <thomasgollick@gmail.com>
2025-11-05 15:27:58 +0000

    Removing unused dependencies

--- src/app/admin/applications/applicationsTable.tsx
+++ src/app/admin/applications/applicationsTable.tsx
@@ -3,18 +3,12 @@
 import React from "react";
 import { DataTable } from "./data-table";
 import { columns, Contact } from "./columns";
-import { trpc } from "../../../../utils/providers/TrpcProviders";
-import { ssrTrpc } from "@/backend/trpc/ssr-caller";
-
 
 interface ApplicationsTableProps {
   initialData: Contact[];
 }
 
 const ApplicationsTable = ({ initialData }: ApplicationsTableProps) => {
-
-
-
   return <DataTable data={initialData || []} columns={columns} />;
 };
 
